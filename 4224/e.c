037ec9ed4dfdf7e22be395f5a68319037aa55d7c
mohan730 <p.mohan730@gmail.com>
2019-09-21 12:03:00 +0530

    firstcommit

---
+++ 4224/e.c
@@ -0,0 +1,11 @@
+#include<at89c51xd2.h>
+#include<stdio.h>
+void main()
+{
+	int a=5,b=6;
+	P0=a+b;
+	P1=b-a;
+	P2=a*b;
+	P3=b/a;
+	
+}
---
+++ 4224/ex.c
@@ -0,0 +1,8 @@
+#include<at89c51xd2.h>
+#include<stdio.h>
+void main()
+{
+	int a=5,b=6;
+	P0=a+b;
+	
+}
